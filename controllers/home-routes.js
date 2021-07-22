dde8328ce8ad0e1e13dafad2366f12e440a4ed25
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-07-22 01:30:36 -0400

    helper functions added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,7 @@ router.get('/', (req, res) => {
     // pass a single post object into the homepage template
     const posts = dbPostData.map(post => post.get({plain: true}));
     res.render('homepage', {
-      post,
+      posts,
       loggedIn: req.session.loggedIn
     });
   })
